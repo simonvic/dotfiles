872a14c606991d85c4ca1a7cb82f02fa4aaf29b6
simonvic <simonvic@live.it>
2020-03-15 16:33:25 +0100

    Update brightness.sh
    Former-commit-id: 6cd277beeea7cff5a5033cd02f03b00e72f51988 [formerly 669bd5521265e252bb5dc37fe61fd4c4e0fd4031]
    Former-commit-id: d441a31a6399f3b77249a658337347a1cd6ce81b

--- i3/scripts/brightness.sh
+++ i3/scripts/brightness.sh
@@ -1,35 +1,81 @@
 #!/bin/bash
 
-function get_brightness {
+source ~/.config/polybar/scripts/env.sh
+
+#	Increase the volume by x%
+increaseAmount=1
+
+#	With drawBox = true
+#	┌────────────────────┐
+#	│████████████▃░░░░░░░│ 62%
+#	└────────────────────┘
+
+#	With drawBox = false
+#	████████████▃░░░░░░░ 62%
+drawBox=true
+
+#	Default icon
+icon=/usr/share/icons/Paper/scalable/status/display-brightness-medium-symbolic.svg
+
+#	Default urgency level [Available: low, normal, critical]
+urgency=normal
+
+#	For how much milliseconds the notification will stay visible
+timeout=1500
+
+#	Unique dunst notification id
+uid=2594
+
+line=$(seq -s "─" 0 5 100 | sed 's/[0-9]//g')
+
+function getBrightness {
     xbacklight -get
 }
 
-function send_notification {
-    brightness=`get_brightness`
-    # Make the bar with the special character ─ (it's not dash -)
-    # https://en.wikipedia.org/wiki/Box-drawing_character
-	 if [ $(( ($brightness-4) % 5 )) -eq 0 ]; then
-	 		midBar=▆
-	 	elif [ $(( ($brightness-3) % 5 )) -eq 0 ]; then
-			midBar=▅
-	 	elif [ $(( ($brightness-2) % 5 )) -eq 0 ]; then
-			midBar=▃
-	 	elif [ $(( ($brightness-1) % 5 )) -eq 0 ]; then
-			midBar=▁
-	 fi
+function sendNotification {
+    brightness=`getBrightness`	
+    
+    #	Building the brightness bar
+	if [ $(( ($brightness-4) % 5 )) -eq 0 ]; then
+		midBar=▆
+	elif [ $(( ($brightness-3) % 5 )) -eq 0 ]; then
+		midBar=▅
+	elif [ $(( ($brightness-2) % 5 )) -eq 0 ]; then
+		midBar=▃
+	elif [ $(( ($brightness-1) % 5 )) -eq 0 ]; then
+		midBar=▁
+	fi
     bar=$(seq -s "█" 0 5 $brightness | sed 's/[0-9]//g'  )
-    emptyBar=$(seq -s "░" $(( ((100-$brightness) /5)+1 )) | sed 's/[0-9]//g'  )
+    emptyBar=$(seq -s "░" $(( ((100-$brightness) /5)+1 )) | sed 's/[0-9]//g')	
+    
+   	if [ $drawBox = true ]; then    
+		if [ "$REDSHIFT" = "on" ]; then
+			body="┌$line┐\t<b>$brightness%</b>\n│$bar$midBar$emptyBar│\n└$line┘\t<b>$REDSHIFT_TEMP\k</b>"    	
+		else
+	 	    body="┌$line┐\n│$bar$midBar$emptyBar│\t<b>$brightness%</b>\n└$line┘"
+		fi
+    else
+    	if [ "$REDSHIFT" = "on" ]; then
+			body="$bar$midBar$emptyBar <b>$brightness% $REDSHIFT_TEMP\k</b>"    	
+		else
+	 	    body="$bar$midBar$emptyBar <b>$brightness%</b>"
+		fi
+    fi
+    if [ $brightness -gt 50 ]; then
+		icon=/usr/share/icons/Paper/scalable/status/display-brightness-high-symbolic.svg
+	fi
     # Send the notification
-    dunstify -a "Brightness" -i xxx -t 1000 -r 2593 -u normal "$brightness%" "$bar$midBar$emptyBar"
+    dunstify -a "Brightness" -i "$icon" -t "$timeout" -r "$uid" -u "$urgency" "$brightness%" "$body"
+    
 }
 
 case $1 in
     up)
-     xbacklight -inc 10 # -step 10
-	  send_notification
+     xbacklight -inc 1
+	  sendNotification
 	;;
     down)
-     xbacklight -dec 10 # -step 10
-	  send_notification
+     xbacklight -dec 1
+	  sendNotification
 	;;
 esac
