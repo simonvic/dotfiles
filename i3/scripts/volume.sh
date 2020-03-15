37c0585d3385939f4ac48c4c1443a1bf21530497
simonvic <simonvic@live.it>
2020-03-15 16:34:19 +0100

    Update volume.sh
    Former-commit-id: e88bf8a80a99db148f31b9e03f743ebb8bac2ff3 [formerly 501871bcefec817a277db155ea43a5a58bf397db]
    Former-commit-id: 1f8bc661da5cfb9b8abc00da4b84e094a4b52332

--- i3/scripts/volume.sh
+++ i3/scripts/volume.sh
@@ -1,60 +1,104 @@
 #!/bin/bash
 
-# You can call this script like this:
-# $./volume.sh up
-# $./volume.sh down
-# $./volume.sh mute
+#	Increase the volume by x%
+increaseAmount=1
 
-function get_volume {
+#	With drawBox = true
+#	┌────────────────────┐
+#	│████████████▃░░░░░░░│ 62%
+#	└────────────────────┘
+
+#	With drawBox = false
+#	████████████▃░░░░░░░ 62%
+drawBox=false
+
+#	Default icon
+icon=audio-volume-medium
+
+#	Default urgency level [Available: low, normal, critical]
+urgency=normal
+
+#	At which level the volume is considered too high
+warningLevel=60
+
+#	For how much milliseconds the notification will stay visible
+timeout=1500
+
+#	Unique dunst notification id
+uid=2593
+
+
+line=$(seq -s "─" 0 5 100 | sed 's/[0-9]//g')
+
+
+
+function getVolume {
     amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
 }
 
-function is_mute {
+function isMute {
     amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
 }
 
-#▁ ▂ ▃ ▄ ▅ ▆ ▇ █
+function sendNotification {
+    volume=`getVolume`
 
-function send_notification {
-    volume=`get_volume`
-    # Make the bar with the special character ─ (it's not dash -)
-    # https://en.wikipedia.org/wiki/Box-drawing_character
-	 if [ $(( ($volume-4) % 5 )) -eq 0 ]; then
-	 		midBar=▆
-	 	elif [ $(( ($volume-3) % 5 )) -eq 0 ]; then
-			midBar=▅
-	 	elif [ $(( ($volume-2) % 5 )) -eq 0 ]; then
-			midBar=▃
-	 	elif [ $(( ($volume-1) % 5 )) -eq 0 ]; then
-			midBar=▁
-	 fi
-    bar=$(seq -s "█" 0 5 $volume | sed 's/[0-9]//g'  )
-    emptyBar=$(seq -s "░" $(( ((100-$volume) /5)+1 )) | sed 's/[0-9]//g'  )
+	# Building the volume bar
+	# TO-DO make colored warning bar
+    bar=$(seq -s "█" 0 5 $volume | sed 's/[0-9]//g') 
+	if [ $(( ($volume-4) % 5 )) -eq 0 ]; then
+		midBar=▆
+	elif [ $(( ($volume-3) % 5 )) -eq 0 ]; then
+		midBar=▅
+	elif [ $(( ($volume-2) % 5 )) -eq 0 ]; then
+		midBar=▃
+	elif [ $(( ($volume-1) % 5 )) -eq 0 ]; then
+		midBar=▁
+	fi
+    emptyBar=$(seq -s "░" $(( ((100-$volume) /5)+1 )) | sed 's/[0-9]//g')
+   
+   	if [ $drawBox = true ]; then
+ 	 body="┌$line┐\n│$bar$midBar$emptyBar│\t<b>$volume%</b>\n└$line┘"	 
+	else
+	 body="$bar$midBar$emptyBar\t<b>$volume%</b>"
+ 	fi
+
+	if [ $volume -ge $warningLevel ]; then
+  		urgency=critical
+  		icon=audio-volume-high
+  	fi
+  	
     # Send the notification
-    dunstify -a "Volume" -i audio-volume-high -t 1000 -r 2593 -u normal "$volume%" "$bar$midBar$emptyBar"
+    dunstify -a "Volume" -i "$icon" -t "$timeout" -r "$uid" -u "$urgency" "$volume" "$body"
+    
 }
 
 case $1 in
     up)
 	# Set the volume on (if it was muted)
 	amixer -D pulse set Master on > /dev/null
-	# Up the volume (+ 5%)
-	amixer -D pulse sset Master 5%+ > /dev/null
-	send_notification
+	amixer -D pulse sset Master "$increaseAmount"%+ > /dev/null
+	sendNotification
 	;;
     down)
 	amixer -D pulse set Master on > /dev/null
-	amixer -D pulse sset Master 5%- > /dev/null
-	send_notification
+	amixer -D pulse sset Master "$increaseAmount"%- > /dev/null
+	sendNotification
 	;;
     mute)
     	# Toggle mute
 	amixer -D pulse set Master 1+ toggle > /dev/null
-	if is_mute ; then
-		emptyBar=$(seq -s "░" $((100/5 +1)) | sed 's/[0-9]//g'  )
-		dunstify -a "Volume" -i audio-volume-low -t 1000 -r 2593 -u normal "Muted" "$emptyBar"
+	if isMute ; then
+		emptyBar=$(seq -s "░" $((100/5 +1)) | sed 's/[0-9]//g')
+		
+		if [ $drawBox = true ]; then
+			body="┌$line┐\n│$emptyBar│\t<b>Muted</b>\n└$line┘"
+		else
+			body="$emptyBar\t<b>Muted</b>"
+		fi
+		dunstify -a "Volume" -i audio-volume-low -t "$timeout" -r "$uid" -u "$urgency" "Muted" "$body"
 	else
-	    send_notification
+	    sendNotification
 	fi
 	;;
 esac
