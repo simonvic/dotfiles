76e9549349797f3f8b84368eb61005c000527ba1
simonvic <simonvic@live.it>
2020-04-07 19:18:00 +0200

    Update battery.sh
    Former-commit-id: 4f700e7237b67e818c62ae2ac63bcb7429891e96 [formerly 709fae9edf79da1bf371af6b9c6e7253e684c3b9]
    Former-commit-id: 0b75d1ae27cc89c499bdceaa587357650431b652

--- battery-manager/battery.sh
+++ battery-manager/battery.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+### Call this script in a crontab (preferably every minute)
+### EXAMPLE of my crontab:
+### 	* * * * * DISPLAY=':0' DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/1000/bus' $HOME/Documents/Programming/scripts/battery-manager/battery.sh monitor
+
 #	Default icon
 icon=battery-medium
 
