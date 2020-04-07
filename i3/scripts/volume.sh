602c0f20dcb840462dbcf743df52f1f2c04b62f9
simonvic <simonvic@live.it>
2020-04-07 19:08:18 +0200

    sound added + optimization
    Former-commit-id: f7c6d03719cc3e38cf9b0d6208162febb05a26cd [formerly 561f782109b33fb11e04be19601fc8e38a068c6e]
    Former-commit-id: f06d794dd7adcb654585dc7489e82b98c6e6149d

--- i3/scripts/volume.sh
+++ i3/scripts/volume.sh
@@ -10,7 +10,15 @@
 drawBox=false
 
 #	Default icon
-icon=/usr/share/icons/Paper/24x24/panel/audio-volume-medium.svg
+mutedIcon=/usr/share/icons/Vimix-Ruby/22/actions/audio-volume-muted.svg
+lowIcon=/usr/share/icons/Vimix-Ruby/22/actions/audio-volume-low.svg
+icon=/usr/share/icons/Vimix-Ruby/22/actions/audio-volume-medium.svg
+warningIcon=/usr/share/icons/Vimix-Ruby/22/actions/audio-volume-high.svg
+
+# Enable to play sound
+playSound=true
+# Sound to play (usually located in /usr/share/sounds/)
+sound=/usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
 
 #	Default urgency level [Available: low, normal, critical]
 urgency=normal
@@ -38,7 +46,7 @@ function isMute {
 }
 
 function sendNotification {
-    volume=`getVolume`
+  volume=`getVolume`
 
 	# Building the volume bar
 	# TO-DO make colored warning bar
@@ -55,47 +63,52 @@ function sendNotification {
     emptyBar=$(seq -s "░" $(( ((100-$volume) /5)+1 )) | sed 's/[0-9]//g')
    
    	if [ $drawBox = true ]; then
- 	 body="┌$line┐\n│$bar$midBar$emptyBar│\t<b>$volume%</b>\n└$line┘"	 
+ 	 body="┌$line┐\n│$bar$midBar$emptyBar│<b>$volume%</b>\n└$line┘\t"	 
 	else
-	 body="$bar$midBar$emptyBar\t<b>$volume%</b>"
+	 body="$bar$midBar$emptyBar\t<b>$volume%</b>\t"
  	fi
 
 	if [ $volume -ge $warningLevel ]; then
-  		urgency=critical
-		icon=/usr/share/icons/Paper/24x24/panel/audio-volume-high.svg
-  	fi
-  	
-    # Send the notification
-    dunstify -a "Volume" -i "$icon" -t "$timeout" -r "$uid" -u "$urgency" "$volume" "$body"
-    
+  	urgency=critical
+		icon=$warningIcon
+	elif [ $volume -le 20 ]; then
+		icon=$lowIcon
+	fi
+	
+  # Send the notification
+  dunstify -a "Volume" -i "$icon" -t "$timeout" -r "$uid" -u "$urgency" "$volume" "$body"
+  
+  if [ $playSound = true ]; then
+		paplay $sound
+	fi
 }
 
 case $1 in
-    up)
-	# Set the volume on (if it was muted)
-	amixer -D pulse set Master on > /dev/null
-	amixer -D pulse sset Master "$2"%+ > /dev/null
-	sendNotification
+	up)
+		# Set the volume on (if it was muted)
+		amixer -D pulse set Master on > /dev/null
+		amixer -D pulse sset Master "$2"%+ > /dev/null
+		sendNotification
 	;;
-    down)
-	amixer -D pulse set Master on > /dev/null
-	amixer -D pulse sset Master "$2"%- > /dev/null
-	sendNotification
+	down)
+		amixer -D pulse set Master on > /dev/null
+		amixer -D pulse sset Master "$2"%- > /dev/null
+		sendNotification
 	;;
-    mute)
-    	# Toggle mute
-	amixer -D pulse set Master 1+ toggle > /dev/null
-	if isMute ; then
-		emptyBar=$(seq -s "░" $((100/5 +1)) | sed 's/[0-9]//g')
-		
-		if [ $drawBox = true ]; then
-			body="┌$line┐\n│$emptyBar│\t<b>Muted</b>\n└$line┘"
+	mute)
+ 	# Toggle mute
+		amixer -D pulse set Master 1+ toggle > /dev/null
+		if isMute ; then
+			emptyBar=$(seq -s "░" $((100/5 +1)) | sed 's/[0-9]//g')
+			
+			if [ $drawBox = true ]; then
+				body="┌$line┐\n│$emptyBar│\t<b>Muted</b>\n└$line┘"
+			else
+				body="$emptyBar\t<b>Muted</b>"
+			fi
+			dunstify -a "Volume" -i $mutedIcon -t "$timeout" -r "$uid" -u "$urgency" "Muted" "$body"
 		else
-			body="$emptyBar\t<b>Muted</b>"
+			  sendNotification
 		fi
-		dunstify -a "Volume" -i /usr/share/icons/Paper/24x24/panel/audio-volume-low.svg -t "$timeout" -r "$uid" -u "$urgency" "Muted" "$body"
-	else
-	    sendNotification
-	fi
 	;;
 esac
