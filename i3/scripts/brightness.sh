9fae7ef59f95c5ee279a04f28b0af0d4aa072e2c
simonvic <simonvic@live.it>
2020-04-07 19:07:07 +0200

    Update brightness.sh
    Former-commit-id: 6dd1f614b32252ad9369a9252f4d8ed73ea74aa0 [formerly 7b72f621fa194f00ae82c3f4fc73a74d5ed1327e]
    Former-commit-id: 4e2199c71c9f366f3fcba0a5f3e20e37c830d24f

--- i3/scripts/brightness.sh
+++ i3/scripts/brightness.sh
@@ -13,7 +13,14 @@ source ~/.config/polybar/scripts/env.sh
 drawBox=false
 
 #	Default icon
-icon=/usr/share/icons/Paper/scalable/status/display-brightness-medium-symbolic.svg
+lowIcon=/usr/share/icons/Vimix-Ruby/symbolic/status/display-brightness-low-symbolic.svg
+icon=/usr/share/icons/Vimix-Ruby/symbolic/status/display-brightness-medium-symbolic.svg
+highIcon=/usr/share/icons/Vimix-Ruby/symbolic/status/display-brightness-high-symbolic.svg
+
+# Enable to play sound
+playSound=true
+# Sound to play (usually located in /usr/share/sounds/)
+sound=/usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
 
 #	Default urgency level [Available: low, normal, critical]
 urgency=normal
@@ -31,7 +38,7 @@ function getBrightness {
 }
 
 function sendNotification {
-    brightness=`getBrightness`	
+   brightness=`getBrightness`	
     
     #	Building the brightness bar
 	if [ $(( ($brightness-4) % 5 )) -eq 0 ]; then
@@ -43,28 +50,35 @@ function sendNotification {
 	elif [ $(( ($brightness-1) % 5 )) -eq 0 ]; then
 		midBar=▁
 	fi
-    bar=$(seq -s "█" 0 5 $brightness | sed 's/[0-9]//g'  )
-    emptyBar=$(seq -s "░" $(( ((100-$brightness) /5)+1 )) | sed 's/[0-9]//g')	
-    
-   	if [ $drawBox = true ]; then    
+  bar=$(seq -s "█" 0 5 $brightness | sed 's/[0-9]//g'  )
+  emptyBar=$(seq -s "░" $(( ((100-$brightness) /5)+1 )) | sed 's/[0-9]//g')	
+  
+ 	if [ $drawBox = true ]; then    
 		if [ "$REDSHIFT" = "on" ]; then
 			body="┌$line┐\t<b>$brightness%</b>\n│$bar$midBar$emptyBar│\n└$line┘\t<b>$REDSHIFT_TEMP\k</b>"    	
 		else
-	 	    body="┌$line┐\n│$bar$midBar$emptyBar│\t<b>$brightness%</b>\n└$line┘"
+		  body="┌$line┐\n│$bar$midBar$emptyBar│\t<b>$brightness%</b>\n└$line┘"
 		fi
-    else
-    	if [ "$REDSHIFT" = "on" ]; then
+  else
+  	if [ "$REDSHIFT" = "on" ]; then
 			body="$bar$midBar$emptyBar <b>$brightness% $REDSHIFT_TEMP\k</b>"    	
 		else
-	 	    body="$bar$midBar$emptyBar <b>$brightness%</b>"
+ 	   	body="$bar$midBar$emptyBar <b>$brightness%</b>"
 		fi
-    fi
-    if [ $brightness -gt 50 ]; then
-		icon=/usr/share/icons/Paper/scalable/status/display-brightness-high-symbolic.svg
+  fi
+  
+  if [ $brightness -ge 60 ]; then
+		icon=$highIcon
+	elif [ $brightness -le 30 ]; then
+		icon=$lowIcon
 	fi
+	
     # Send the notification
     dunstify -a "Brightness" -i "$icon" -t "$timeout" -r "$uid" -u "$urgency" "$brightness%" "$body"
     
+    if [ $playSound = true ]; then
+ 			paplay $sound
+		fi
 }
 
 case $1 in
