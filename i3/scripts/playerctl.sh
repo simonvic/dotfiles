8248744acaa31aa2ffcbe743df3d3fbc12de13bf
simonvic <simonvic@live.it>
2020-04-09 23:04:45 +0200

    Update playerctl.sh
    Former-commit-id: 3bff4fa0f8f452a8a8599de28c2d09addefd094f [formerly 57818d0a4092f0c69fa9ac1c3f9959d7c5dea7c5]
    Former-commit-id: ee2da44875e263f2b90e7cc7afedce5f6b4c8d19

--- i3/scripts/playerctl.sh
+++ i3/scripts/playerctl.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+###########################################
+#	Made by
+#       _                              _        
+#      (_)                            (_)       
+#  ___  _  ____    ___   ____   _   _  _   ____ 
+# /___)| ||    \  / _ \ |  _ \ | | | || | / ___)
+#|___ || || | | || |_| || | | | \ V / | |( (___ 
+#(___/ |_||_|_|_| \___/ |_| |_|  \_/  |_| \____)
+
+#	Check updates and give a look at my dotfiles here:
+#		https://github.com/simonvic/dotfiles
+
+###########################################
+
+
 #	Default player to control
 #	TO-DO make shortcut to change player on the fly
 player=spotify
