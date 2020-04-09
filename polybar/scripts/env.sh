1c12e224600aacdea094774c5f43f6e89450ea56
simonvic <simonvic@live.it>
2020-04-09 23:20:49 +0200

    Update env.sh
    Former-commit-id: 81d16a375f0e99ef34af647a595132df5b99a5a3 [formerly ffbb63d85370f47b62c6a5f708d5375c03c360a7]
    Former-commit-id: 11df853796ca1a61f8da679a4ad731d7588e7c53

--- polybar/scripts/env.sh
+++ polybar/scripts/env.sh
@@ -1,2 +1,16 @@
+###########################################
+#	Made by
+#       _                              _        
+#      (_)                            (_)       
+#  ___  _  ____    ___   ____   _   _  _   ____ 
+# /___)| ||    \  / _ \ |  _ \ | | | || | / ___)
+#|___ || || | | || |_| || | | | \ V / | |( (___ 
+#(___/ |_||_|_|_| \___/ |_| |_|  \_/  |_| \____)
+
+#	Check updates and give a look at my dotfiles here:
+#		https://github.com/simonvic/dotfiles
+
+###########################################
+
 export REDSHIFT=on
-export REDSHIFT_TEMP=3500
+export REDSHIFT_TEMP=2800
