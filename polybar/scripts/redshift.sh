c8808260f51086a674c41f06fbe3b13c2c2424fb
simonvic <simonvic@live.it>
2020-04-09 23:22:16 +0200

    Update redshift.sh
    Former-commit-id: a26669f88c61760e487fef6dab9621fc1b94d055 [formerly e050c01f51a04c94fcb207ce5f39b98f4df3f8f7]
    Former-commit-id: 4f71c32354ddaa1075cc754a8f56a3da07956a17

--- polybar/scripts/redshift.sh
+++ polybar/scripts/redshift.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+###########################################
+#	Made by
+#       _                              _        
+#      (_)                            (_)       
+#  ___  _  ____    ___   ____   _   _  _   ____ 
+# /___)| ||    \  / _ \ |  _ \ | | | || | / ___)
+#|___ || || | | || |_| || | | | \ V / | |( (___ 
+#(___/ |_||_|_|_| \___/ |_| |_|  \_/  |_| \____)
+
+#	Check updates and give a look at my dotfiles here:
+#		https://github.com/simonvic/dotfiles
+
+###########################################
+
 envFile=~/.config/polybar/scripts/env.sh
-changeValue=300
+changeValue=100
 
 changeMode() {
   sed -i "s/REDSHIFT=$1/REDSHIFT=$2/g" $envFile 
