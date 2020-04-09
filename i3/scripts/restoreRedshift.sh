9057a4e434bc50d59cf2640937f74efe2d7b44c9
simonvic <simonvic@live.it>
2020-04-09 23:05:33 +0200

    Update restoreRedshift.sh
    Former-commit-id: 6d28b0502027a3778a0dcf507f749c9f8e1e2cf1 [formerly d634e5db2d3e3ba77080022c19c5ad85e943f917]
    Former-commit-id: 28e7a90b72425afe56a498a41ae76191da15284f

--- i3/scripts/restoreRedshift.sh
+++ i3/scripts/restoreRedshift.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+###########################################
+#	Made by
+#       _                              _        
+#      (_)                            (_)       
+#  ___  _  ____    ___   ____   _   _  _   ____ 
+# /___)| ||    \  / _ \ |  _ \ | | | || | / ___)
+#|___ || || | | || |_| || | | | \ V / | |( (___ 
+#(___/ |_||_|_|_| \___/ |_| |_|  \_/  |_| \____)
+
+#	Check updates and give a look at my dotfiles here:
+#		https://github.com/simonvic/dotfiles
+
+###########################################
+
 source ~/.config/polybar/scripts/env.sh
 
 if [ "$REDSHIFT" = "on" ]; then
