dfbf31bdf3241d309c8dd3c2826561a58196c5f6
simonvic <simonvic@live.it>
2020-04-12 16:06:23 +0200

    Update restoreRedshift.sh
    Former-commit-id: 2508313ea0b27536f46413ce6ef86db7f293d331 [formerly 05089d680c2c903888676c093516217bdfefe6e2]
    Former-commit-id: ea9fc83f706999db94f8c90cb38da3f89726fcf7

--- i3/scripts/restoreRedshift.sh
+++ i3/scripts/restoreRedshift.sh
@@ -14,8 +14,8 @@
 
 ###########################################
 
-source ~/.config/polybar/scripts/env.sh
+source ~/.config/i3/scripts/redshift.sh
 
-if [ "$REDSHIFT" = "on" ]; then
+if [ "$REDSHIFT_STATUS" = "on" ]; then
 	redshift -O "$REDSHIFT_TEMP"
 fi
