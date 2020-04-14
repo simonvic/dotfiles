485677c48c5e429e056c2d5b35214f93d3e28ea4
simonvic <simonvic@live.it>
2020-04-14 19:05:58 +0200

    Update todolist.sh
    Former-commit-id: 22c794e048bd9d4f188703ff100a0f5883ea0f9a [formerly 14f0e3521f85b5b6164674d932c7917a66d46abc]
    Former-commit-id: 6aef68286429531ab9bc59226a7fa93c34a2e450

--- polybar/scripts/todolist.sh
+++ polybar/scripts/todolist.sh
@@ -1,10 +1,11 @@
 #TO-DO optimization
-text=$(cat $HOME/Documents/Programming/scripts/todo/todo.txt | grep  | sed -e ':a;N;$!ba;s/\n/      /g')
-totalChar=$(wc -c $HOME/Documents/Programming/scripts/todo/todo.txt | cut -d " " -f1 )
-charToShow=$totalChar
-for i in $(seq $totalChar)
-do
-	echo "TO-DO: ${text:$i:$charToShow}"
-	sleep 1
+while true; do
+	text=$(cat $HOME/Documents/Programming/scripts/todo/todo.txt | grep  | sed -e ':a;N;$!ba;s/\n/      /g')
+	totalChar=$(wc -c $HOME/Documents/Programming/scripts/todo/todo.txt | cut -d " " -f1 )
+	charToShow=$totalChar
+	for i in $(seq $totalChar)
+	do
+		echo "TO-DO: ${text:$i:$charToShow}"
+		sleep 1
+	done
 done
-
