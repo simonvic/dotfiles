abd7a3bd4917b63d6bcea9a2dbb62146d0a59018
simonvic <simonvic@live.it>
2020-04-18 16:06:30 +0200

    Update brightness.sh
    Former-commit-id: 89bf749e057918a4a0d6b47ab2ea947a4e2e5ed9 [formerly ae3e02537f6c4dccadc2e776ba2e09a326a7aac8]
    Former-commit-id: dbff9b93af5c8e4fbd8eba82d9ca37b4ae3ceec0

--- i3/scripts/brightness.sh
+++ i3/scripts/brightness.sh
@@ -15,16 +15,6 @@
 ###########################################
 
 
-# Draw box in notification
-#	With drawBox = true
-#	┌────────────────────┐
-#	│████████████▃░░░░░░░│ 62%
-#	└────────────────────┘
-
-#	With drawBox = false
-#	████████████▃░░░░░░░ 62%
-drawBox=false
-
 #	Default icons
 lowIcon=/usr/share/icons/Vimix-Ruby/symbolic/status/display-brightness-low-symbolic.svg
 icon=/usr/share/icons/Vimix-Ruby/symbolic/status/display-brightness-medium-symbolic.svg
@@ -97,49 +87,32 @@ function changeTemp() {
   fi
 }
 
+function buildBar {
+	$HOME/.config/i3/scripts/drawBar.sh $1 $2 $3
+}
+
 function sendNotification {
-   brightness=`getBrightness`	
-    
-    #	Building the brightness bar
-	if [ $(( ($brightness-4) % 5 )) -eq 0 ]; then
-		midBar=▆
-	elif [ $(( ($brightness-3) % 5 )) -eq 0 ]; then
-		midBar=▅
-	elif [ $(( ($brightness-2) % 5 )) -eq 0 ]; then
-		midBar=▃
-	elif [ $(( ($brightness-1) % 5 )) -eq 0 ]; then
-		midBar=▁
+	brightness=`getBrightness`	
+ 	# Building the volume bar
+	if [ "$REDSHIFT_STATUS" = "on" ]; then
+		body="`buildBar 5 $brightness false` <b>$brightness% $REDSHIFT_TEMP\k</b>"    	
+	else
+		body="`buildBar 5 $brightness false` <b>$brightness%</b>"
 	fi
-  bar=$(seq -s "█" 0 5 $brightness | sed 's/[0-9]//g'  )
-  emptyBar=$(seq -s "░" $(( ((100-$brightness) /5)+1 )) | sed 's/[0-9]//g')	
-  
- 	if [ $drawBox = true ]; then   
- 		line=$(seq -s "─" 0 5 100 | sed 's/[0-9]//g') 
-		if [ "$REDSHIFT_STATUS" = "on" ]; then
-			body="┌$line┐\t<b>$brightness%</b>\n│$bar$midBar$emptyBar│\n└$line┘\t<b>$REDSHIFT_TEMP\k</b>"    	
-		else
-		  body="┌$line┐\n│$bar$midBar$emptyBar│\t<b>$brightness%</b>\n└$line┘"
-		fi
-  else
-  	if [ "$REDSHIFT_STATUS" = "on" ]; then
-			body="$bar$midBar$emptyBar <b>$brightness% $REDSHIFT_TEMP\k</b>"    	
-		else
- 	   	body="$bar$midBar$emptyBar <b>$brightness%</b>"
-		fi
-  fi
   
   if [ $brightness -ge 60 ]; then
 		icon=$highIcon
 	elif [ $brightness -le 30 ]; then
 		icon=$lowIcon
 	fi
-	
-    # Send the notification
-    dunstify -a "Brightness" -i "$icon" -t "$timeout" -r "$uid" -u "$urgency" "$brightness%" "$body"
-    
-    if [ $playSound = true ]; then
- 			paplay $sound &
-		fi
+
+	if [ $playSound = true ]; then
+		paplay $sound &
+	fi
+  
+  # Send the notification
+  dunstify -a "Brightness" -i "$icon" -t "$timeout" -r "$uid" -u "$urgency" "$brightness%" "$body"
+  
 }
 
 function printBrightness() {
