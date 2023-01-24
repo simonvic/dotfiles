b0f6423691ce258c7c31f01eb4bfb846561e3fad
simonvic <simonvic.dev@gmail.com>
2023-01-24 23:06:13 +0100

    feat(nvim): improve jdtls config
    - debug plugin version is not hardcoded anymore
    - added test plugin (from vscode)
    - improve root_directory detection for maven projects
    - added config file for formatting option

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,15 +1,62 @@
-require("jdtls").start_or_attach({
+local vim = vim
+local jdtls = require("jdtls")
+vim.api.nvim_create_user_command(
+	"JTestClass",
+	function(input)
+		jdtls.test_class()
+	end,
+	{}
+)
+vim.api.nvim_create_user_command(
+	"JTestMethod",
+	function(input)
+		jdtls.test_nearest_method()
+	end,
+	{}
+)
+
+local bundles = {
+	vim.fn.glob("~/.m2/repository/com/microsoft/java/com.microsoft.java.debug.plugin/*/com.microsoft.java.debug.plugin-*.jar"
+		, 1)
+}
+
+vim.list_extend(bundles,
+	vim.split(vim.fn.glob("~/.vscode-oss/extensions/vscjava.vscode-java-test-*/server/*.jar", 1), "\n"))
+
+jdtls.start_or_attach({
 	cmd = { "/usr/bin/jdtls" },
-	root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" }, { upward = true })[1]),
+	root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw", "pom.xml" }, { upward = true })[1]),
 	init_options = {
-		bundles = {
-			vim.fn.glob("~/.m2/repository/com/microsoft/java/com.microsoft.java.debug.plugin/0.42.0/com.microsoft.java.debug.plugin-0.42.0.jar"
-				, 1)
-		}
+		bundles = bundles
 	},
 	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 	on_attach = function(client, bufnr)
 		require("jdtls").setup_dap({ hotcode_replace = "auto" })
 		require("jdtls.setup").add_commands()
-	end
+	end,
+	settings = {
+		java = {
+			format = {
+				settings = {
+					url = "~/.config/jdtls/settings.xml"
+				}
+			},
+			-- configuration = {
+			-- 	runtimes = {
+			-- 		{
+			-- 			name = "JavaSE-11",
+			-- 			path = "/usr/lib/jvm/java-11-openjdk/",
+			-- 		},
+			-- 		{
+			-- 			name = "JavaSE-17",
+			-- 			path = "/usr/lib/jvm/java-17-openjdk/",
+			-- 		},
+			-- 		{
+			-- 			name = "JavaSE-19",
+			-- 			path = "/usr/lib/jvm/java-19-openjdk/",
+			-- 		},
+			-- 	}
+			-- }
+		}
+	}
 })
