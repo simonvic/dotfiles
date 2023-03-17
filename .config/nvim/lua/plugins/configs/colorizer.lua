819d0405487cda6c676e4ec9bb2222a17cef157c
simonvic <simonvic.dev@gmail.com>
2023-03-17 13:19:10 +0100

    feat(nvim): recognition of rrggbb colors

--- .config/nvim/lua/plugins/configs/colorizer.lua
+++ .config/nvim/lua/plugins/configs/colorizer.lua
@@ -1,13 +1,14 @@
 return function()
 	require("colorizer").setup({
-		filetype = {"*"},
+		filetype = { "*" },
 		user_default_options = {
-			names = false,
+			names    = false,
+			RRGGBB   = true,
 			RRGGBBAA = true,
 			AARRGGBB = true,
-			css = false,
-			css_fn = true,
-			mode = "background"
+			css      = false,
+			css_fn   = true,
+			mode     = "background"
 		}
 	})
 end
