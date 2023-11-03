004491deb8c5dc61c8d302b80eac340dcd6770e1
simonvic <simonvic.dev@gmail.com>
2023-11-03 21:57:16 +0100

    fix(nvim): use default notification position (top)

--- .config/nvim/lua/plugins/configs/notify.lua
+++ .config/nvim/lua/plugins/configs/notify.lua
@@ -2,7 +2,6 @@ return function()
 	local notify = require("notify")
 	notify.setup({
 		stages = "slide",
-		top_down = false
 	})
 	vim.notify = notify
 	require("telescope").load_extension("notify")
