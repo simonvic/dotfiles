c1e65b331208a639040609a970b4d0208a45c077
simonvic <simonvic.dev@gmail.com>
2023-12-12 20:05:13 +0100

    fix(nvim): update jdk version for jdtls

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -33,8 +33,8 @@ jdtls.start_or_attach({
 			configuration = {
 				runtimes = {
 					{
-						name = "JavaSE-17",
-						path = "/usr/lib/jvm/java-17-openjdk/",
+						name = "JavaSE-21",
+						path = "/usr/lib/jvm/java-21-openjdk/",
 					},
 				},
 			},
