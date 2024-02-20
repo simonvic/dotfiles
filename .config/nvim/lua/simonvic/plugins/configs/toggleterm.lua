6911d1d884135ac37422a26d0ec933a661f31c89
simonvic <simonvic.dev@gmail.com>
2024-02-20 12:00:23 +0100

    fix(nvim): disable toggleterm custom winbar

--- .config/nvim/lua/simonvic/plugins/configs/toggleterm.lua
+++ .config/nvim/lua/simonvic/plugins/configs/toggleterm.lua
@@ -4,20 +4,10 @@ return function()
 		shading_factor = 2,
 		direction = "horizontal",
 		highlights = {
-			Normal = { link = "Normal" }
+			WinBar = { link = "WinBar" }
 		},
 		float_opts = {
 			border = "curved",
-			highlights = {
-				border = "Normal",
-				background = "Normal",
-			}
-		},
-		winbar = {
-			enabled = true,
-			name_formatter = function(term)
-				return term.name
-			end
 		},
 	})
 end
