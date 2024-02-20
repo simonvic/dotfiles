724e363de5663fc6a4888336388b2e3fa7b30b59
simonvic <simonvic.dev@gmail.com>
2024-02-20 12:08:39 +0100

    feat(nvim): custom lsp handlers for lua and html

--- .config/nvim/lua/simonvic/plugins/configs/mason-lspconfig.lua
+++ .config/nvim/lua/simonvic/plugins/configs/mason-lspconfig.lua
@@ -5,8 +5,40 @@ return function()
 	local default_capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 	mason_lspconfig.setup_handlers({
+
+		-- Default handler
 		function(server_name)
 			require("lspconfig")[server_name].setup({ capabilities = default_capabilities })
 		end,
+
+		-- Per-language handlers
+
+		["lua_ls"] = function(server_name)
+			require("lspconfig")[server_name].setup({
+				capabilities = default_capabilities,
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" }, },
+						workspace = { library = vim.api.nvim_get_runtime_file("", true), },
+						telemetry = { enable = false, },
+					}
+				}
+			})
+		end,
+
+		["html"] = function(server_name)
+			require("lspconfig").html.setup({
+				capabilities = default_capabilities,
+				settings = {
+					html = {
+						format = {
+							indentInnerHtml = true,
+							contentUnformatted = "pre,code,textarea"
+						}
+					}
+				}
+			})
+		end
+
 	})
 end
