83b877c4fc1bd54d199eb12f97fd455606111e67
simonvic <simonvic.dev@gmail.com>
2024-09-18 12:13:45 +0200

    feat(nvim): repeatable ts-textobjects movements

--- .config/nvim/lua/simonvic/plugins/configs/ts-textobjects.lua
+++ .config/nvim/lua/simonvic/plugins/configs/ts-textobjects.lua
@@ -77,4 +77,13 @@ return function()
 			},
 		},
 	})
+
+	local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+	vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_next)
+	vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_previous)
+	vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+	vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+	vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+	vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+
 end
