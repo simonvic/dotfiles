471df30b019b9640061a386a974f5e39b191f838
simonvic <simonvic.dev@gmail.com>
2024-10-22 18:23:14 +0200

    feat(nvim): telescope keybinding to send all entries to qflist

--- .config/nvim/lua/simonvic/plugins/configs/nvim-telescope.lua
+++ .config/nvim/lua/simonvic/plugins/configs/nvim-telescope.lua
@@ -8,6 +8,7 @@ return function()
 					["<C-h>"] = actions.select_horizontal,
 					["<C-v>"] = actions.select_vertical,
 					["<A-CR>"] = actions.send_selected_to_qflist,
+					["<C-CR>"] = actions.send_to_qflist,
 				}
 			}
 		},
