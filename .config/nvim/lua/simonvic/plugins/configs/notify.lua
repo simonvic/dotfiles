70806dfd9e6d19580572164afaa5c606f884b7d0
simonvic <simonvic.dev@gmail.com>
2024-10-22 18:27:17 +0200

    feat(nvim): use compact notifications style

--- .config/nvim/lua/simonvic/plugins/configs/notify.lua
+++ .config/nvim/lua/simonvic/plugins/configs/notify.lua
@@ -2,6 +2,7 @@ return function()
 	local notify = require("notify")
 	notify.setup({
 		stages = "slide",
+		render = "compact"
 	})
 	vim.notify = notify
 	require("telescope").load_extension("notify")
