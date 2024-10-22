e5ecbe776fd0b5a2a7534f39d4b7102d3adff0a0
simonvic <simonvic.dev@gmail.com>
2024-10-22 18:27:55 +0200

    feat(nvim): add lldb and c configurations for DAP

--- .config/nvim/lua/simonvic/plugins/configs/debugger.lua
+++ .config/nvim/lua/simonvic/plugins/configs/debugger.lua
@@ -1,5 +1,22 @@
 return function()
 	local dap = require("dap")
+	dap.adapters.lldb = {
+		type = 'executable',
+		command = '/usr/bin/lldb-dap',
+		name = 'lldb'
+	}
+	dap.configurations.c = {
+		{
+			name = 'Launch',
+			type = 'lldb',
+			request = 'launch',
+			-- program = vim.fn.getcwd() .. '/target/a.out',
+			program = function() return require("dap.utils").pick_file() end,
+			cwd = '${workspaceFolder}',
+			stopOnEntry = false,
+			args = {},
+		}
+	}
 	local signs = require("simonvic.signs").plugins.dap
 	for name, sign in pairs(signs) do
 		-- sign.numhl = sign.texthl
