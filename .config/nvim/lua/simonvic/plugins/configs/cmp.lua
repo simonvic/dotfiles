410cfc87673874aa05d63c42b6ac84874062bd9f
simonvic <simonvic.dev@gmail.com>
2025-03-07 15:14:41 +0100

    fix(nvim): cmp sources priorities

--- .config/nvim/lua/simonvic/plugins/configs/cmp.lua
+++ .config/nvim/lua/simonvic/plugins/configs/cmp.lua
@@ -42,11 +42,11 @@ return function()
 			})
 		},
 		sources = {
-			{ group_index = 1, name = "nvim_lsp" },
 			{ group_index = 1, name = "nvim_lsp_signature_help" },
-			{ group_index = 1, name = "luasnip" },
-			{ group_index = 2, name = "buffer" },
-			{ group_index = 2, name = "path" },
+			{ group_index = 2, name = "nvim_lsp" },
+			{ group_index = 3, name = "luasnip" },
+			{ group_index = 4, name = "buffer" },
+			{ group_index = 4, name = "path" },
 		},
 		-- TODO: cmp-git
 		preselect = cmp.PreselectMode.None,
