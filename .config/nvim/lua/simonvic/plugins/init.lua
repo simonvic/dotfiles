2f24b85aa1dd97e596cbe6bb1ef40cd256f39b57
simonvic <simonvic.dev@gmail.com>
2025-03-29 18:42:25 +0100

    fix(nvim): use vim.uv instead of deprecated vim.loop

--- .config/nvim/lua/simonvic/plugins/init.lua
+++ .config/nvim/lua/simonvic/plugins/init.lua
@@ -89,7 +89,7 @@ end
 
 function M.with_lazy()
 	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not vim.loop.fs_stat(lazypath) then
+	if not vim.uv.fs_stat(lazypath) then
 		vim.fn.system({
 			"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
 		})
