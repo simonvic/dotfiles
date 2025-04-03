ea7ff31c5e13cf7e46f2034867e203cd30d28bc1
simonvic <simonvic.dev@gmail.com>
2025-04-03 17:28:42 +0200

    feat(nvim): java textblock autopair

--- .config/nvim/lua/simonvic/plugins/configs/autopairs.lua
+++ .config/nvim/lua/simonvic/plugins/configs/autopairs.lua
@@ -1,6 +1,12 @@
 return function()
-	require("nvim-autopairs").setup({
+	local npairs = require("nvim-autopairs")
+	npairs.setup({
 		enable_check_bracket_line = false,
 		check_ts = true
+	})
+	local Rule = require("nvim-autopairs.rule")
+	local cond = require("nvim-autopairs.conds")
+	npairs.add_rules({
+	       Rule('"""', '"""', { "java" }):with_pair(cond.not_before_char('"', 3))
 	})
 end
