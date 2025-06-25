fbca1a5bb1bd1e773fe22255e1eafb93b0e52ee4
simonvic <simonvic.dev@gmail.com>
2025-06-25 11:49:02 +0200

    fix(nvim): improve treesitter indentation support detection

--- .config/nvim/lua/simonvic/plugins/configs/treesitter.lua
+++ .config/nvim/lua/simonvic/plugins/configs/treesitter.lua
@@ -1,10 +1,13 @@
 return function()
-	-- Use nvim-treesitter indentation expr
+	-- Use nvim-treesitter indentation expr if available
 	vim.api.nvim_create_autocmd("FileType", {
 		pattern = { "*" },
 		callback = function(args)
 			local lang = vim.treesitter.language.get_lang(args.match)
-			if lang and vim.treesitter.language.add(lang) then
+			if lang
+				and vim.treesitter.language.add(lang)
+				and #vim.api.nvim_get_runtime_file("queries/" .. lang .. "/indents.scm", false) >= 1
+			then
 				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 			end
 		end,
