0794e226cd05f944fa33a67218f5d714f3bd9134
simonvic <simonvic.dev@gmail.com>
2025-08-15 21:51:44 +0200

    fix(nvim): don't set highlights on scrollbar plugin setup

--- .config/nvim/lua/simonvic/plugins/configs/scrollbar.lua
+++ .config/nvim/lua/simonvic/plugins/configs/scrollbar.lua
@@ -1,17 +1,6 @@
 return function()
-	local c = require("simonvic.colorscheme").groups
 	require("scrollbar").setup({
-		handle = {
-			color = c.ScrollbarHandle.bg
-		},
-		marks = {
-			Search = { color = c.ScrollbarSearchHandle.fg },
-			Error  = { color = c.ScrollbarErrorHandle.fg },
-			Warn   = { color = c.ScrollbarWarnHandle.fg },
-			Info   = { color = c.ScrollbarInfoHandle.fg },
-			Hint   = { color = c.ScrollbarHintHandle.fg },
-			Misc   = { color = c.ScrollbarMiscHandle.fg },
-		},
+		set_highlights = false,
 		handlers = {
 			cursor = false
 		},
