a14e68aadbfc2c9004911993cd007366779880dd
simonvic <simonvic.dev@gmail.com>
2025-09-26 14:32:41 +0200

    feat(nvim): use right side window layout

--- .config/nvim/lua/simonvic/plugins/configs/undotree.lua
+++ .config/nvim/lua/simonvic/plugins/configs/undotree.lua
@@ -1,5 +1,6 @@
 return function()
 	vim.g.undotree_SetFocusWhenToggle = true
+	vim.g.undotree_WindowLayout = 3
 	require("simonvic.keybindings").implement({
 		undotree = vim.cmd.UndotreeToggle
 	})
