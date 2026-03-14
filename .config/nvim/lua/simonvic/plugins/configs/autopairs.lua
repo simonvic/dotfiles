153a2ccd9ec576999893eee923178f5e452c58e8
simonvic <simonvic.dev@gmail.com>
2026-03-14 15:26:49 +0100

    feat(nvim): add new autopairs rule for latex and giovi

--- .config/nvim/lua/simonvic/plugins/configs/autopairs.lua
+++ .config/nvim/lua/simonvic/plugins/configs/autopairs.lua
@@ -7,6 +7,11 @@ return function()
 	local Rule = require("nvim-autopairs.rule")
 	local cond = require("nvim-autopairs.conds")
 	npairs.add_rules({
-	       Rule('"""', '"""', { "java" }):with_pair(cond.not_before_char('"', 3))
+		Rule('"""', '"""', { "java" }):with_pair(cond.not_before_char('"', 3)),
+		Rule('$', '$', { "tex", "latex" }),
+		Rule('\\[', '\\]', { "tex", "latex" }),
+		Rule("<", ">", { "giovi" }):with_pair(cond.before_text(".")),
+		Rule("#{?", "}#", { "giovi" }):with_pair(cond.not_after_text("}#")),
+		Rule("#{", "}#", { "giovi" }),
 	})
 end
