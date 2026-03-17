a97ff1ae73d203c221383999a7a380201a1b0e79
simonvic <simonvic.dev@gmail.com>
2026-03-17 12:50:08 +0100

    feat(nvim): add custom giovi colorscheme

---
+++ .config/nvim/colors/giovi.lua
@@ -0,0 +1,35 @@
+require("simonvic.colorscheme.ruby"):build({
+	name = "giovi",
+	groups = function(p)
+		return {
+
+			["@comment.giovi"]                     = { fg = p.text_dark, italic = false },
+			["@comment.block.giovi"]               = { fg = p.text_xdark, italic = false },
+			["@comment.documentation.giovi"]       = { fg = "#738796", italic = true },
+			["@comment.documentation.block.giovi"] = { fg = "#596974", italic = true },
+
+			["@number.natural.giovi"]              = { fg = "#7C69BB" },
+			["@number.giovi"]                      = { fg = "#6869BB" },
+			["@number.float.giovi"]                = { fg = "#4A69C5" },
+
+			["@nontext.giovi"]                     = { bg = "none" },
+			["@string.giovi"]                      = { fg = "#6A8759" },
+			["@string.textblock.giovi"]            = { bg = "#283422", fg = "#5A734B" },
+			["@string.block.giovi"]                = { fg = "#5A734B" },
+			["@string.exact.giovi"]                = { fg = "#6A8777" },
+			["@string.exact.block.giovi"]          = { fg = "#566D61" },
+
+			["@punctuation.giovi"]                 = { fg = "#F37973" },
+			["@operator.giovi"]                    = { fg = "#F05938" },
+
+			["@constant.builtin.giovi"]            = { fg = p.metakeyword },
+			["@variable.builtin.giovi"]            = { fg = p.keyword_light, italic = true },
+			["@type.giovi"]                        = { fg = "#FAFAFA", bold = true },
+			["@label.giovi"]                       = { fg = p.text_xdark },
+			-- ["@error.giovi"]                       = { link = "DiagnosticUnderlineError" },
+
+			["TSCurrentScope"]                     = { bg = "#333333" },
+
+		}
+	end
+}):apply()
