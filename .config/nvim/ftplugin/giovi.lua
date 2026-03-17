2159e10736099c813511c3ef1efbfee23f1ff6e5
simonvic <simonvic.dev@gmail.com>
2026-03-17 12:49:06 +0100

    feat(nvim): set giovi filetype options

---
+++ .config/nvim/ftplugin/giovi.lua
@@ -0,0 +1,17 @@
+vim.opt.commentstring = "# %s"
+vim.opt.formatoptions = "jcroql"
+vim.opt.comments = {
+
+	-- "s:#{?",
+	-- "m:#?",
+	-- "ex:}#",
+
+	-- "s:#{",
+	-- "m:#",
+	-- "e:}#",
+
+	"b:#?",
+	"b:#",
+}
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
