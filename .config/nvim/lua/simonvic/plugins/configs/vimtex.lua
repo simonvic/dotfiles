cf9d25299e43ba44e19d71cd7c5c61907057431a
simonvic <simonvic.dev@gmail.com>
2026-09-22 17:58:18 +0200

    feat(nvim/vimtex): explicitly enable ampersand indentation

--- .config/nvim/lua/simonvic/plugins/configs/vimtex.lua
+++ .config/nvim/lua/simonvic/plugins/configs/vimtex.lua
@@ -1,4 +1,12 @@
 return function()
 	vim.g.vimtex_view_general_viewer = "zathura"
 	vim.g.vimtex_view_method = "zathura_simple"
+	vim.g.vimtex_indent_on_ampersands = 1
+	-- vim.g.vimtex_compiler_latexmk = { options = { "-shell-escape" } }
+	-- vim.g.vimtex_compiler_latexmk_engines = {
+	-- 	['_'] = '-lualatex', -- or '-xelatex' for xelatex
+	-- 	['pdflatex'] = '-pdf',
+	-- 	['lualatex'] = '-lualatex',
+	-- 	['xelatex'] = '-xelatex',
+	-- }
 end
